ed4c54e8bd6ecb1597e2fa25200b3825b8c30a6c
Patrick Kolze <patrick_kolze@gmx.net>
2017-02-05 23:58:42 +0100

    soso

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -7,14 +7,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class Application {
-
+public class Application extends SpringBootServletInitializer{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
@@ -29,5 +33,4 @@ public class Application {
 
         };
     }
-
 }
